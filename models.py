1a6dd073f39a23a973099f870b298c6a7ab446f1
alihosseini1387-hub <ali.hosseini1387@icloud.com>
2026-08-01 17:01:28 +0330

    Update models.py

--- models.py
+++ models.py
@@ -126,7 +126,7 @@ def calc_Kf_nonlinear(Kf0, deltaP):
     if deltaP < 12:
         return Kf0
     else:
-        exponent = 0.05 *(20-12)*( (deltaP - 12)/((20-12))**2)
+        exponent = 0.05 * (20 - 11) * ((deltaP - 11) / (20 - 11))**2
         return Kf0 * np.exp(exponent)
 
 
